9273e28b7e4e826cbe1378adb25c5efa9410e6d8
Rodrigo M. Rios <rios_r.m@hotmail.com>
2023-05-12 00:25:19 -0300

    commit
    Commit para atualização do Footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menu.addEventListener("click", function(){
+menu.addEventListener("click", function () {
   navbar.classList.toggle("active");
 });
 
